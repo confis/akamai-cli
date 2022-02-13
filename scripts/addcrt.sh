4dd562398e26f2d2117862703fbdbde8e1acd8e8
Yaniv Cohen <confis@gmail.com>
2022-02-13 16:55:37 +0200

    Update addcrt.sh

--- scripts/addcrt.sh
+++ scripts/addcrt.sh
@@ -4,4 +4,7 @@ File= "/home/runner/work/akamai-cli/akamai-cli/domain/domainlist.txt"
  do
    cat ./scripts/sample.yml | sed 's/foo.com/'$line'/g' > ./crt/$line.yml
    echo "domain: $line"
+   cd /home/runner/work/akamai-cli/akamai-cli/crt
+   git add $line.yml
+   git commit -m "new subdomain added"   
  done < "$File"
